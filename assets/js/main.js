d9100a417cd8308b11b535f94cbee9d14a5eb195
Abraham <abrahamluccas13@gmail.com>
2023-07-06 17:26:39 -0300

    load button/style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,32 @@
-
-function PokemonTypesToList(pokemonTypes){
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
-function PokemonToList(pokemon){
-    return ` <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
-            <span class="name">${pokemon.name}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    ${PokemonTypesToList(pokemon.types).join('')}
-                </ol>
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
-            </div>
-            
-        </li>
-    `
-}
-
 const pokemonList = document.getElementById('pokemonList');
+const loadmoreButton = document.getElementById('loadmore_btn')
+const limit = 6;
+let offset = 0;
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(PokemonToList).join('')
-})
+function loadPokemonItens(offset, limit){
+
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHTML = pokemons.map((pokemon) => ` 
+        <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${pokemon.photo}" 
+            alt="${pokemon.name}">
+        </div>
         
+        </li>
+            `).join('')
+        pokemonList.innerHTML += newHTML
+    })
+}
+
+loadPokemonItens(offset, limit)
+        
+loadmoreButton.addEventListener('click', () => {
+    offset += limit;
+    loadPokemonItens(offset, limit)
+})
